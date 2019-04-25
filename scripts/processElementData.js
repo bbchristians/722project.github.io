82305180695666709b59fc9e68f5645531090b51
Ben Christians <bbc7909@rit.edu>
2019-04-25 17:32:50 -0400

    updates

---
+++ scripts/processElementData.js
@@ -0,0 +1,128 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMA,_9dkncWYQEemD98_zFpAUawActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/Deliver Final Version_C143179E_ddc112b8_Activity.jpeg"
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMA,_z1RQcWYQEemD98_zFpAUawActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/Elicit Customer Feedback_7BA38F26_98218a40_Activity.jpeg"
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMA,_zmYnQWYNEemD98_zFpAUawActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/Preliminary Software_3162E3E7_4de0df01_Activity.jpeg"
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMA,_wtIHEV76EemGWvwDoSVWMAActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/software_concept_2BF626AA_487421c4_Activity.jpeg"
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMA,_ASoa8WYPEemD98_zFpAUawActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/Design Architecture and System Core_EDCC9C3E_a4a9758_Activity.jpeg"
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMAActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/evolutionary_delivery_C117DEC6_c117dec6_Activity.jpeg"
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMA,_QdmPkWUQEemmFZ-BlWtqCQActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/deliver_the_version_60A3C3C_22883756_Activity.jpeg"
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMA,_MvDC4GYQEemD98_zFpAUawActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/Develop a Version_2FCA7777_4c487291_Activity.jpeg"
+contentPage.processPage.imageFiles["_m3pPsV76EemGWvwDoSVWMA,_3kpsQGYQEemD98_zFpAUawActivity"]="./../../SnowayPlugin/deliveryprocesses/resources/Incorporate Customer Feedback_9FECC0CD_bc6abbe7_Activity.jpeg"
+contentPage.processPage.elementUrls["_PnaXIWYQEemD98_zFpAUaw"] = ["QA Analyst","SnowayPlugin/deliveryprocesses/qa_analyst_A578B57F.html"];
+contentPage.processPage.elementUrls["_Iktl5mYPEemD98_zFpAUaw"] = ["Developer","SnowayPlugin/deliveryprocesses/developer_4DB0462D.html"];
+contentPage.processPage.elementUrls["_SWLMuWUOEemmFZ-BlWtqCQ"] = ["Vision Document","SnowayPlugin/deliveryprocesses/vision_document_2471731F.html"];
+contentPage.processPage.elementUrls["_pOLNQGUQEemmFZ-BlWtqCQ"] = ["Develop User Acceptance Test Plan","SnowayPlugin/deliveryprocesses/develop_user_acceptance_test_plan_CF0F74F1.html"];
+contentPage.processPage.elementUrls["_Iktl5GYPEemD98_zFpAUaw"] = ["QA Analyst","SnowayPlugin/deliveryprocesses/qa_analyst_54795207.html"];
+contentPage.processPage.elementUrls["_PnZwEGYQEemD98_zFpAUaw"] = ["Identify prioritized requirements to address in iteration","SnowayPlugin/deliveryprocesses/identify_prioritized_requirements_to_address_in_iteration_1AC846B9.html"];
+contentPage.processPage.elementUrls["_IkuNAGYPEemD98_zFpAUaw"] = ["Approval From Architect","SnowayPlugin/deliveryprocesses/approval_from_architect_58FA4D3C.html"];
+contentPage.processPage.elementUrls["_m3pPsV76EemGWvwDoSVWMA"] = ["Evolutionary Delivery","SnowayPlugin/deliveryprocesses/evolutionary_delivery_C117DEC6.html"];
+contentPage.processPage.elementUrls["_vPUQUGUQEemmFZ-BlWtqCQ"] = ["Execute User Acceptance Tests","SnowayPlugin/deliveryprocesses/execute_user_acceptance_tests_A2C20142.html"];
+contentPage.processPage.elementUrls["_G6ptQmYOEemD98_zFpAUaw"] = ["Vision Document","SnowayPlugin/deliveryprocesses/vision_document_99DF02E9.html"];
+contentPage.processPage.elementUrls["_pOLNR2UQEemmFZ-BlWtqCQ"] = ["Implemented Version","SnowayPlugin/deliveryprocesses/implemented_version_F5B220FB.html"];
+contentPage.processPage.elementUrls["_vPUQU2UQEemmFZ-BlWtqCQ"] = ["User Acceptance Test Feedback","SnowayPlugin/deliveryprocesses/user_acceptance_test_feedback_4D2CB2D.html"];
+contentPage.processPage.elementUrls["_B17HyWYREemD98_zFpAUaw"] = ["User Documentation","SnowayPlugin/deliveryprocesses/user_documentation_A3B8897B.html"];
+contentPage.processPage.elementUrls["_SWLMt2UOEemmFZ-BlWtqCQ"] = ["Requirements Gathering Plan","SnowayPlugin/deliveryprocesses/requirements_gathering_plan_B7527ADB.html"];
+contentPage.processPage.elementUrls["_2mD9F2YQEemD98_zFpAUaw"] = ["User Acceptance Test Feedback","SnowayPlugin/deliveryprocesses/user_acceptance_test_feedback_B9FA05B8.html"];
+contentPage.processPage.elementUrls["_G6ofIWYOEemD98_zFpAUaw"] = ["Requirements Analysis","SnowayPlugin/deliveryprocesses/requirements_analysis_5FC3AEEE.html"];
+contentPage.processPage.elementUrls["_IkuM8mYPEemD98_zFpAUaw"] = ["Formal Requirements Document","SnowayPlugin/deliveryprocesses/formal_requirements_document_9B65EC8A.html"];
+contentPage.processPage.elementUrls["_wtIHEV76EemGWvwDoSVWMA"] = ["Software Concept","SnowayPlugin/deliveryprocesses/software_concept_2BF626AA.html"];
+contentPage.processPage.elementUrls["_SWLMsGUOEemmFZ-BlWtqCQ"] = ["Define Vision","SnowayPlugin/deliveryprocesses/define_vision_90AFCED1.html"];
+contentPage.processPage.elementUrls["_6JdLVGYQEemD98_zFpAUaw"] = ["Project Manager","SnowayPlugin/deliveryprocesses/project_manager_3A14C4C9.html"];
+contentPage.processPage.elementUrls["_PnaXM2YQEemD98_zFpAUaw"] = ["Test Execution Results","SnowayPlugin/deliveryprocesses/test_execution_results_F3327D6.html"];
+contentPage.processPage.elementUrls["_PnaXK2YQEemD98_zFpAUaw"] = ["Code Documentation","SnowayPlugin/deliveryprocesses/code_documentation_860F6398.html"];
+contentPage.processPage.elementUrls["_IkuM9GYPEemD98_zFpAUaw"] = ["Architecture Outline Document","SnowayPlugin/deliveryprocesses/architecture_outline_document_66C0DA83.html"];
+contentPage.processPage.elementUrls["_6JckQGYQEemD98_zFpAUaw"] = ["Impact Analysis of New Requirements","SnowayPlugin/deliveryprocesses/impact_analysis_of_new_requirements_633B5A2E.html"];
+contentPage.processPage.elementUrls["_SWLMtWUOEemmFZ-BlWtqCQ"] = ["Initial Authorization","SnowayPlugin/deliveryprocesses/initial_authorization_5FDF9100.html"];
+contentPage.processPage.elementUrls["_6JdLWmYQEemD98_zFpAUaw"] = ["Risk Chart","SnowayPlugin/deliveryprocesses/risk_chart_F7DD9B0E.html"];
+contentPage.processPage.elementUrls["_Iks-1mYPEemD98_zFpAUaw"] = ["Validate Preliminary Builds with Stakeholders","SnowayPlugin/deliveryprocesses/validate_preliminary_builds_with_stakeholders_B6D8D8D3.html"];
+contentPage.processPage.elementUrls["_pOLNRWUQEemmFZ-BlWtqCQ"] = ["Formal Requirements Document","SnowayPlugin/deliveryprocesses/formal_requirements_document_9E3F3720.html"];
+contentPage.processPage.elementUrls["_G6ofIGYOEemD98_zFpAUaw"] = ["Identify Risks","SnowayPlugin/deliveryprocesses/identify_risks_5525CEDE.html"];
+contentPage.processPage.elementUrls["_IkuM9mYPEemD98_zFpAUaw"] = ["Technology Recommendations","SnowayPlugin/deliveryprocesses/technology_recommendations_5FF7CEA9.html"];
+contentPage.processPage.elementUrls["_G6ofImYOEemD98_zFpAUaw"] = ["Requirements Prioritization","SnowayPlugin/deliveryprocesses/requirements_prioritization_4E5CC304.html"];
+contentPage.processPage.elementUrls["_G6qUUWYOEemD98_zFpAUaw"] = ["Risk Chart","SnowayPlugin/deliveryprocesses/risk_chart_BD8D774F.html"];
+contentPage.processPage.elementUrls["_Iks-0mYPEemD98_zFpAUaw"] = ["Identify Opportunities for Reuse and Integration","SnowayPlugin/deliveryprocesses/identify_opportunities_for_reuse_and_integration_F246F6B4.html"];
+contentPage.processPage.elementUrls["_SWLMs2UOEemmFZ-BlWtqCQ"] = ["Project Manager","SnowayPlugin/deliveryprocesses/project_manager_F2C098BC.html"];
+contentPage.processPage.elementUrls["_2mD9GWYQEemD98_zFpAUaw"] = ["Bug Report","SnowayPlugin/deliveryprocesses/bug_report_2718FDFC.html"];
+contentPage.processPage.elementUrls["_Iktl6GYPEemD98_zFpAUaw"] = ["Stakeholder","SnowayPlugin/deliveryprocesses/stakeholder_190B3426.html"];
+contentPage.processPage.elementUrls["_B17Hw2YREemD98_zFpAUaw"] = ["Project Manager","SnowayPlugin/deliveryprocesses/project_manager_7207AF18.html"];
+contentPage.processPage.elementUrls["_Iktl4mYPEemD98_zFpAUaw"] = ["System Architect","SnowayPlugin/deliveryprocesses/system_architect_891E640E.html"];
+contentPage.processPage.elementUrls["_G6qUVWYOEemD98_zFpAUaw"] = ["Prioritized Requirements","SnowayPlugin/deliveryprocesses/prioritized_requirements_821F596E.html"];
+contentPage.processPage.elementUrls["_PnaXI2YQEemD98_zFpAUaw"] = ["Formal Requirements Document","SnowayPlugin/deliveryprocesses/formal_requirements_document_FCEB9F5A.html"];
+contentPage.processPage.elementUrls["_G6pGNmYOEemD98_zFpAUaw"] = ["Stakeholder","SnowayPlugin/deliveryprocesses/stakeholder_25362D9F.html"];
+contentPage.processPage.elementUrls["_PnaXKWYQEemD98_zFpAUaw"] = ["Test Plan","SnowayPlugin/deliveryprocesses/test_plan_2E9C79BD.html"];
+contentPage.processPage.elementUrls["_PnaXL2YQEemD98_zFpAUaw"] = ["Implemented Version","SnowayPlugin/deliveryprocesses/implemented_version_4AA145B7.html"];
+contentPage.processPage.elementUrls["_2mD9EGYQEemD98_zFpAUaw"] = ["Clean and Analyze feedback","SnowayPlugin/deliveryprocesses/clean_and_analyze_feedback_935759AE.html"];
+contentPage.processPage.elementUrls["_2mD9G2YQEemD98_zFpAUaw"] = ["Requirements Change Request","SnowayPlugin/deliveryprocesses/requirements_change_request_7E8BE7D7.html"];
+contentPage.processPage.elementUrls["_Dc_VcWUPEemmFZ-BlWtqCQ"] = ["Initial Requirements Document","SnowayPlugin/deliveryprocesses/initial_requirements_document_CD60E2C3.html"];
+contentPage.processPage.elementUrls["_z1RQcWYQEemD98_zFpAUaw"] = ["Elicit Customer Feedback","SnowayPlugin/deliveryprocesses/Elicit Customer Feedback_7BA38F26.html"];
+contentPage.processPage.elementUrls["_6JdLXGYQEemD98_zFpAUaw"] = ["Prioritized Requirements","SnowayPlugin/deliveryprocesses/prioritized_requirements_C3388907.html"];
+contentPage.processPage.elementUrls["_Iks-1WYPEemD98_zFpAUaw"] = ["Implement Risky Requirements","SnowayPlugin/deliveryprocesses/implement_risky_requirements_C83FC4BD.html"];
+contentPage.processPage.elementUrls["_IkuM_GYPEemD98_zFpAUaw"] = ["System Architecture","SnowayPlugin/deliveryprocesses/system_architecture_94686B1D.html"];
+contentPage.processPage.elementUrls["_6JdLUmYQEemD98_zFpAUaw"] = ["System Architect","SnowayPlugin/deliveryprocesses/system_architect_6EB9D6D0.html"];
+contentPage.processPage.elementUrls["_Dc_VcGUPEemmFZ-BlWtqCQ"] = ["Generate Requirements","SnowayPlugin/deliveryprocesses/generate_requirements_C2C302B3.html"];
+contentPage.processPage.elementUrls["_PnZwE2YQEemD98_zFpAUaw"] = ["Project Manager","SnowayPlugin/deliveryprocesses/project_manager_7CD910A4.html"];
+contentPage.processPage.elementUrls["_Iks-0GYPEemD98_zFpAUaw"] = ["Design Architecture","SnowayPlugin/deliveryprocesses/design_architecture_F910028E.html"];
+contentPage.processPage.elementUrls["_IkuM-mYPEemD98_zFpAUaw"] = ["Reuse Reccomendation","SnowayPlugin/deliveryprocesses/reuse_reccomendation_29213535.html"];
+contentPage.processPage.elementUrls["_B17HwGYREemD98_zFpAUaw"] = ["Distribution of Software","SnowayPlugin/deliveryprocesses/distribution_of_software_FF6E52D.html"];
+contentPage.processPage.elementUrls["_B17HxWYREemD98_zFpAUaw"] = ["Final Functional Version","SnowayPlugin/deliveryprocesses/final_functional_version_DF26A75C.html"];
+contentPage.processPage.elementUrls["_vLhAUWUOEemmFZ-BlWtqCQ"] = ["Stakeholder","SnowayPlugin/deliveryprocesses/stakeholder_1DA203C9.html"];
+contentPage.processPage.elementUrls["_vPUQUWUQEemmFZ-BlWtqCQ"] = ["Stakeholder","SnowayPlugin/deliveryprocesses/stakeholder_AD5FE152.html"];
+contentPage.processPage.elementUrls["_B17Hx2YREemD98_zFpAUaw"] = ["Configuration Plan","SnowayPlugin/deliveryprocesses/configuration_plan_36999137.html"];
+contentPage.processPage.elementUrls["_Iktl4GYPEemD98_zFpAUaw"] = ["Project Manager","SnowayPlugin/deliveryprocesses/project_manager_8FE76FE8.html"];
+contentPage.processPage.elementUrls["_G6ptQGYOEemD98_zFpAUaw"] = ["Initial Requirements Document","SnowayPlugin/deliveryprocesses/initial_requirements_document_A0A80EC3.html"];
+contentPage.processPage.elementUrls["_SWLMsWUOEemmFZ-BlWtqCQ"] = ["Business Analyst","SnowayPlugin/deliveryprocesses/business_analyst_9B4DAEE1.html"];
+contentPage.processPage.elementUrls["_Iks-02YPEemD98_zFpAUaw"] = ["Identify Technologies","SnowayPlugin/deliveryprocesses/identify_technologies_5B20CC79.html"];
+contentPage.processPage.elementUrls["_PnaXLWYQEemD98_zFpAUaw"] = ["Final Functional Version","SnowayPlugin/deliveryprocesses/final_functional_version_F32E5BDC.html"];
+contentPage.processPage.elementUrls["_pOLNQWUQEemmFZ-BlWtqCQ"] = ["Project Manager","SnowayPlugin/deliveryprocesses/project_manager_D9AD5501.html"];
+contentPage.processPage.elementUrls["_Iks-12YPEemD98_zFpAUaw"] = ["Verify architecture’s adherence to requirements","SnowayPlugin/deliveryprocesses/verify_architectures_adherence_to_requirements_1FB2AE98.html"];
+contentPage.processPage.elementUrls["_IkuM_mYPEemD98_zFpAUaw"] = ["Partially Functional Build","SnowayPlugin/deliveryprocesses/partially_functional_build_8D9F5F43.html"];
+contentPage.processPage.elementUrls["_IkuM8GYPEemD98_zFpAUaw"] = ["Approval to Begin Development","SnowayPlugin/deliveryprocesses/approval_to_begin_development_A22EF864.html"];
+contentPage.processPage.elementUrls["_3kpsQGYQEemD98_zFpAUaw"] = ["Incorporate Customer Feedback","SnowayPlugin/deliveryprocesses/Incorporate Customer Feedback_9FECC0CD.html"];
+contentPage.processPage.elementUrls["_vLhAU2UOEemmFZ-BlWtqCQ"] = ["Requirements Transcript","SnowayPlugin/deliveryprocesses/requirements_transcript_7514EDA4.html"];
+contentPage.processPage.elementUrls["_6JdLUGYQEemD98_zFpAUaw"] = ["Business Analyst","SnowayPlugin/deliveryprocesses/business_analyst_7582E2AA.html"];
+contentPage.processPage.elementUrls["_PnZwEWYQEemD98_zFpAUaw"] = ["Implement the version","SnowayPlugin/deliveryprocesses/implement_the_version_256626C9.html"];
+contentPage.processPage.elementUrls["_PnZwFWYQEemD98_zFpAUaw"] = ["Developer","SnowayPlugin/deliveryprocesses/developer_E9F808E8.html"];
+contentPage.processPage.elementUrls["_PnaXJ2YQEemD98_zFpAUaw"] = ["Iteration Plan","SnowayPlugin/deliveryprocesses/iteration_plan_C17D8179.html"];
+contentPage.processPage.elementUrls["_2mD9FWYQEemD98_zFpAUaw"] = ["Business Analyst","SnowayPlugin/deliveryprocesses/business_analyst_62871BDD.html"];
+contentPage.processPage.elementUrls["_2mD9EWYQEemD98_zFpAUaw"] = ["Developer","SnowayPlugin/deliveryprocesses/developer_9DF539BE.html"];
+contentPage.processPage.elementUrls["_6JckQWYQEemD98_zFpAUaw"] = ["Manage requirements change","SnowayPlugin/deliveryprocesses/manage_requirements_change_6DD93A3E.html"];
+contentPage.processPage.elementUrls["_PnaXMWYQEemD98_zFpAUaw"] = ["Test Cases","SnowayPlugin/deliveryprocesses/test_cases_B7C03DFB.html"];
+contentPage.processPage.elementUrls["_6JdLWGYQEemD98_zFpAUaw"] = ["Requirements Change Request","SnowayPlugin/deliveryprocesses/requirements_change_request_FEA6A6E8.html"];
+contentPage.processPage.elementUrls["_G6pGMmYOEemD98_zFpAUaw"] = ["System Architect","SnowayPlugin/deliveryprocesses/system_architect_60A44B80.html"];
+contentPage.processPage.elementUrls["_PnZwEmYQEemD98_zFpAUaw"] = ["Test to assure that functionality is conforming to requirements","SnowayPlugin/deliveryprocesses/test_to_assure_that_functionality_is_conforming_to_requirements_13FF3ADF.html"];
+contentPage.processPage.elementUrls["_vLhAUGUOEemmFZ-BlWtqCQ"] = ["Gather Requirements","SnowayPlugin/deliveryprocesses/gather_requirements_130423B9.html"];
+contentPage.processPage.elementUrls["_2mD9E2YQEemD98_zFpAUaw"] = ["Project Manager","SnowayPlugin/deliveryprocesses/project_manager_F5682399.html"];
+contentPage.processPage.elementUrls["_IkuM-GYPEemD98_zFpAUaw"] = ["Test Plan","SnowayPlugin/deliveryprocesses/test_plan_2FEA410F.html"];
+contentPage.processPage.elementUrls["_9dkncWYQEemD98_zFpAUaw"] = ["Deliver Final Version","SnowayPlugin/deliveryprocesses/Deliver Final Version_C143179E.html"];
+contentPage.processPage.elementUrls["_Iks-1GYPEemD98_zFpAUaw"] = ["Implement Architecture","SnowayPlugin/deliveryprocesses/implement_architecture_BDA1E4AD.html"];
+contentPage.processPage.elementUrls["_pOLNQ2UQEemmFZ-BlWtqCQ"] = ["QA Analyst","SnowayPlugin/deliveryprocesses/qa_analyst_31203EDC.html"];
+contentPage.processPage.elementUrls["_MvDC4GYQEemD98_zFpAUaw"] = ["Develop a Version","SnowayPlugin/deliveryprocesses/Develop a Version_2FCA7777.html"];
+contentPage.processPage.elementUrls["_G6ofI2YOEemD98_zFpAUaw"] = ["Requirements Validation","SnowayPlugin/deliveryprocesses/requirements_validation_B73698C9.html"];
+contentPage.processPage.elementUrls["_PnaXJWYQEemD98_zFpAUaw"] = ["Partially Functional Build","SnowayPlugin/deliveryprocesses/partially_functional_build_6A0A979E.html"];
+contentPage.processPage.elementUrls["_G6pGMGYOEemD98_zFpAUaw"] = ["Business Analyst","SnowayPlugin/deliveryprocesses/business_analyst_676D575A.html"];
+contentPage.processPage.elementUrls["_G6pGNGYOEemD98_zFpAUaw"] = ["Project Manager","SnowayPlugin/deliveryprocesses/project_manager_2BFF3979.html"];
+contentPage.processPage.elementUrls["_Iktl6mYPEemD98_zFpAUaw"] = ["Business Analyst","SnowayPlugin/deliveryprocesses/business_analyst_1242284C.html"];
+contentPage.processPage.elementUrls["_pOLNTWUQEemmFZ-BlWtqCQ"] = ["User Documentation","SnowayPlugin/deliveryprocesses/user_documentation_2762FB5E.html"];
+contentPage.processPage.elementUrls["_ASoa8WYPEemD98_zFpAUaw"] = ["Design Architecture and System Core","SnowayPlugin/deliveryprocesses/Design Architecture and System Core_EDCC9C3E.html"];
+contentPage.processPage.elementUrls["_pOLNS2UQEemmFZ-BlWtqCQ"] = ["User Acceptance Test Plan","SnowayPlugin/deliveryprocesses/user_acceptance_test_plan_BA44031A.html"];
+contentPage.processPage.elementUrls["_B17HwWYREemD98_zFpAUaw"] = ["Developer","SnowayPlugin/deliveryprocesses/developer_1A94C53D.html"];
+contentPage.processPage.elementUrls["_pOLNSWUQEemmFZ-BlWtqCQ"] = ["Iteration Plan","SnowayPlugin/deliveryprocesses/iteration_plan_62D1193F.html"];
+contentPage.processPage.elementUrls["_Iks-0WYPEemD98_zFpAUaw"] = ["Develop Test Plan","SnowayPlugin/deliveryprocesses/develop_test_plan_3ADE29E.html"];
+contentPage.processPage.elementUrls["_6JdLVmYQEemD98_zFpAUaw"] = ["Formal Requirements Document","SnowayPlugin/deliveryprocesses/formal_requirements_document_334BB8EF.html"];
+contentPage.processPage.elementUrls["_G6qUV2YOEemD98_zFpAUaw"] = ["Approval to Begin Development","SnowayPlugin/deliveryprocesses/approval_to_begin_development_D9924349.html"];
+contentPage.processPage.elementUrls["_G6qUU2YOEemD98_zFpAUaw"] = ["Project Specification","SnowayPlugin/deliveryprocesses/project_specification_1500612A.html"];
+contentPage.processPage.elementUrls["_QdmPkWUQEemmFZ-BlWtqCQ"] = ["Deliver the Version","SnowayPlugin/deliveryprocesses/deliver_the_version_60A3C3C.html"];
+contentPage.processPage.elementUrls["_vPUQVWUQEemmFZ-BlWtqCQ"] = ["Approval for Implemented Version","SnowayPlugin/deliveryprocesses/approval_for_implemented_version_71F1C371.html"];
+contentPage.processPage.elementUrls["_zmYnQWYNEemD98_zFpAUaw"] = ["Preliminary Requirement Analysis","SnowayPlugin/deliveryprocesses/Preliminary Software_3162E3E7.html"];
